03a8ecad26117ca64b2788286e3c3f2e2f6edf54
S3L3CT3DLoves <153332410+S3L3CT3DLoves@users.noreply.github.com>
2024-05-22 22:11:19 +0200

    Update json_info.py
    Update to support jsons v1.2

--- scrapers/JSON_info/json_info.py
+++ scrapers/JSON_info/json_info.py
@@ -11,12 +11,6 @@ sys.path.append(
 )  # add parent dir to sys path so that we can import py_common from there
 
 try:
-    from bencoder import bdecode
-except ModuleNotFoundError:
-    print("You need to install the 'bencoder.pyx' module. (https://pypi.org/project/bencoder.pyx/)", file=sys.stderr)
-    sys.exit()
-
-try:
     from py_common import log, graphql
 except ModuleNotFoundError:
     print("You need to download the folder 'py_common' from the community repo! "
@@ -41,6 +35,7 @@ def get_scene_data(fragment_data):
     }""", {"id": scene_id})
 
     if response and response["findScene"]:
+        log.info(response["findScene"])
         for f in response["findScene"]["files"]:
             scene_files.append({"filename": os.path.basename(f["path"]), "size": f["size"]})
         return {"id": scene_id, "title": scene_title, "files": scene_files}
@@ -61,21 +56,30 @@ def mapValues(scene_data):
     }
 
     #JSON files should contain a "_source" parameter so we can determine their format
-    if(scene_data['_source'] == "SB_AutoDLv1"):
+    formatVersion = float(scene_data['_source'].split('_')[-1][7:])
+
+    if formatVersion >= 1:
         output['title'] = scene_data['title']
         output['date'] = scene_data['date'].split('T')[0]
         output['url'] = scene_data['url']
-        output['code'] = scene_data['id']
         output['image'] = scene_data['image']
         output['tags'] = list(map(lambda x: {"Name":x}, scene_data['tags']))
         output['studio'] = {"Name" : scene_data['studio'] }
+    
+    # Schema v1.1 introduces a field for performers
+    if formatVersion > 1.1:
+        output['performers'] = list(map(lambda x: {"Name":x}, scene_data['performers']))
     return output
 
 
 if sys.argv[1] == "fragment":
     fragment = json.loads(sys.stdin.read())
     scene = get_scene_data(fragment)
-    filename = JSONS_PATH / (scene['files'][0]['filename'].strip(".mp4") + ".json")
-    with open(filename, 'rb') as f:
-        scene_info = json.load(f)
-        print(json.dumps(mapValues(scene_info)))
+    filename = scene['files'][0]['filename'][:-3] + "json"
+    filename = JSONS_PATH / filename
+    try:
+        with open(filename, 'rb') as f:
+            scene_info = json.load(f)
+            print(json.dumps(mapValues(scene_info)))
+    except:
+        print("{}")
